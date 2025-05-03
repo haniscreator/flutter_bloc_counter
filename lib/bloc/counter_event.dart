ce4e90dde768096903f0458b5b5658295c4049ea
pyephyohan <pyephyohan@gmail.com>
2025-05-03 22:43:17 +0700

    Initial commit: Flutter BLoC Counter App

---
+++ lib/bloc/counter_event.dart
@@ -0,0 +1,3 @@
+abstract class CounterEvent {}
+
+class Increment extends CounterEvent {}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/counter_bloc.dart';
+import 'bloc/counter_event.dart';
+import 'bloc/counter_state.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BLoC Counter',
+      home: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: const CounterPage(),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BLoC Counter')),
+      body: Center(
+        child: BlocBuilder<CounterBloc, CounterState>(
+          builder: (context, state) {
+            return Text('Counter: ${state.count}',
+                style: const TextStyle(fontSize: 30));
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<CounterBloc>().add(Increment());
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
